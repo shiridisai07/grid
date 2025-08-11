f2ca2576dddb679acaa21305d445c6539fd1aa76
shiridisai07 <chinnamshiridisai@gmail.com>
2025-08-11 14:56:05 +0530

    Success Message

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/grid/',  // Replace with your actual repo name
+  base: '/grid/', 
   plugins: [react()],
 })
  
